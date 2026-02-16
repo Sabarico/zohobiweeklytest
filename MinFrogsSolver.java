67354de76b0d6fff5f7788d85bc5b75cb6d2847e
Sabarico <55478627+Sabarico@users.noreply.github.com>
2026-02-16 15:05:24 +0530

    Add files via upload

---
+++ MinFrogsSolver.java
@@ -0,0 +1,12 @@
+package practices;
+
+public class MinFrogsSolver {
+public static void minNumberofFrogs(String croakofFrogs) {
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
